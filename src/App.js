69781ed8eca13b8ae64d87d91d78d545032e8f8d
Lucas X <lucasmxisto@outlook.com>
2023-01-24 15:33:20 -0300

    Requisito 1 - Concluído + Lint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
 }
-// Olha a onda, olha a onda!!
 
 export default App;
