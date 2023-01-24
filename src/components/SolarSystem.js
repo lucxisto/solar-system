d5c894bf8d91fa1b83d0dc3db62de76deda6556a
Lucas X <lucasmxisto@outlook.com>
2023-01-24 16:50:33 -0300

    Requisito 6 - Concluído + Lint

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planet, index) => (
+          <PlanetCard
+            key={ index }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
       </div>
     );
   }
