0fad860ef8d1dde13b090a8cd3dbd6eebc069ffd
Lucas X <lucasmxisto@outlook.com>
2023-01-24 22:12:14 -0300

    Adding style

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,21 +2,24 @@ import React from 'react';
 import Title from './Title';
 import MissionCard from './MissionCard';
 import missions from '../data/missions';
+import './Missions.css';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        {missions.map((mission) => (
-          <MissionCard
-            key={ mission.name }
-            name={ mission.name }
-            year={ mission.year }
-            country={ mission.country }
-            destination={ mission.destination }
-          />
-        ))}
+        <div className="mission-container">
+          {missions.map((mission) => (
+            <MissionCard
+              key={ mission.name }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+            />
+          ))}
+        </div>
       </div>
     );
   }
