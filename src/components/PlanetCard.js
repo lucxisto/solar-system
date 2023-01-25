8df495f7c83b996871d05d4b36a5c6a6e80e922a
Lucas X <lucasmxisto@outlook.com>
2023-01-24 22:03:39 -0300

    Adding style

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './PlanetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="planet-container">
         <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+          className="planet-img"
+        />
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import './SolarSystem.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div className="solar-system" data-testid="solar-system">
         <Title headline="Planetas" />
-        {planets.map((planet, index) => (
-          <PlanetCard
-            key={ index }
-            planetName={ planet.name }
-            planetImage={ planet.image }
-          />
-        ))}
+        <div className="planets">
+          {planets.map((planet, index) => (
+            <PlanetCard
+              key={ index }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />
+          ))}
+        </div>
       </div>
     );
   }
